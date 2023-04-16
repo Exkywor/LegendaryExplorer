d503f96628d8f98f58f8c67a348910c410f46687
Exkywor <fernandoquant.dev@gmail.com>
2023-04-16 11:36:05 -0600

    DialogueEd: Fix null interpData bug in new experiments

--- LegendaryExplorer/LegendaryExplorer/Tools/Dialogue Editor/DialogueEditorExperiments/DialogueEditorExperimentsE.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/Dialogue Editor/DialogueEditorExperiments/DialogueEditorExperimentsE.cs
@@ -735,7 +735,7 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
         {
             ExportEntry interpData = node.Interpdata;
 
-            if (TryGetInterp(interpData, out ExportEntry interp))
+            if (interpData != null && TryGetInterp(interpData, out ExportEntry interp))
             {
                 UpdateInterpDataStrRefID(interpData, node.LineStrRef);
                 // Update the Interp comment
@@ -933,6 +933,7 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
         /// <returns>Generated ObjComment array.</returns>
         private static ArrayProperty<StrProperty> GenerateObjComment(string line)
         {
+            line = line.Trim('\"');
             return new("m_aObjComment")
             {
                 new StrProperty(line == "No Data" ? "" : line.Length <= 32 ? line : $"{line.AsSpan(0, 29)}...")
